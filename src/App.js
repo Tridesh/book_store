2b4ce5c8e5f305feb64465539d25805f2211d989
Tridesh <tridesh2005@gmail.com>
2024-03-29 11:46:00 +0530

    update #2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import React from 'react';
 import './App.css';
 import Navbar from './Navbar.js';
+import img from './back-img.png';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <p className='home'>Welcome to LitNook</p>
-      <p className='intro-text'>Explore our curated collection
-       of books and embark on a literary adventure. 
-       Buy your next favorite read here.</p>
+      <div className="image-container">
+        <img className="intro-img" src={img} alt='' />
+        <Navbar />
+      </div>
+      <div className="intro-text-overlay">
+        <p className='home'>Welcome to LitNook</p>
+        <p className='intro-text'>Make your life feeling happy</p>
+      </div>
+      <p className='our_collection'>Our Collection</p>
     </div>
   );
 }
+
 export default App;
